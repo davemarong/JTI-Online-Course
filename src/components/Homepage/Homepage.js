55bf13f5fab76e03ff868875bf5de18c245f3797
davemarong <kmarong@hotmail.com>
2021-10-11 06:55:12 +0200

    Add media queries with vanilla scss instead

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,39 +1,22 @@
 import React from "react";
 // MUI
 import Button from "@mui/material/Button";
-import Grid from "@mui/material/Grid";
-import useMediaQuery from "@mui/material/useMediaQuery";
 
 import styles from "./Homepage.module.scss";
 export default function Homepage() {
-  // Media query
-  const matches = useMediaQuery("(min-width:600px)");
   return (
     <div>
-      {matches ? "True" : "False"}
       <div className={styles.topSection}>
-        <Grid
-          className={styles.gridContainer}
-          container
-          direction={matches ? "row" : "row"}
-          wrap="wrap"
-          justifyContent="center"
-          //   alignItems="center"
-          alignContent="center"
-        >
-          <Grid textAlign="left" item xs={10} sm={7}>
-            <h1>Forstå styrkene til menneskene rundt deg</h1>
-          </Grid>
-          <Grid item xs={10} sm={6}>
-            <p>
-              Bruk JTI til å optimalisere og effektivisere samspill på din
-              arbeidsplass
-            </p>
-          </Grid>
-          <Grid item xs={10} sm={12}>
-            <Button variant="contained">SE KURS</Button>
-          </Grid>
-        </Grid>
+        <h1 className={styles.topSection_h1}>
+          Forstå styrkene til menneskene rundt deg
+        </h1>
+        <p className={styles.topSection_p}>
+          Bruk JTI til å optimalisere og effektivisere samspill på din
+          arbeidsplass
+        </p>
+        <Button className={styles.topSection_button} variant="contained">
+          SE KURS
+        </Button>
       </div>
     </div>
   );
