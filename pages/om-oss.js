26cc2a8a7a535bb9134299672841ac78c33c31f4
davemarong <kmarong@hotmail.com>
2022-07-04 18:18:54 +0200

    Add some content to omjti

--- pages/om-oss.js
+++ pages/om-oss.js
@@ -12,6 +12,23 @@ export default function omOss({
     <div>
       <Nav isLogged={isLogged} />
       <OmOss />
+      <div>
+        <p>
+          Dine preferanser påvirker deg kanskje mer enn du er klar over! Har du
+          lyst til å forstå mer om hvordan dine preferanser påvirker deg, dine
+          handlinger og dine relasjoner? Da er Jung Type Index et godt verktøy.
+          JTI er ikke en personlighetstest, og den måler hverken ferdigheter IQ
+          eller EQ. JTI kartlegger og belyser den enkeltes- eller teamets-
+          preferanser og forskjeller på en analytisk måte. Typeteorien i JTI kan
+          få deg til å betrakte relasjoner, samhandling og kommunikasjon på en
+          ny og spennende måte. JTI som relasjonsverktøy er nyttig for deg som
+          enkeltperson, for ledere, for grupper/ team og for hele
+          organisasjoner. JTI er et anerkjent og internasjonalt verktøy og er
+          utviklet for individplan (JTI) og for team/grupper (Teamkompasset).
+          Begge verktøyene benyttes ofte i forbindelse med
+          organisasjonsutvikling og lederutvikling.
+        </p>
+      </div>
       <Footer />
     </div>
   );
--- src/components/OmJTI/OmJTI.js
+++ src/components/OmJTI/OmJTI.js
@@ -1,5 +1,146 @@
 import React from "react";
 import styles from "./OmJTI.module.scss";
 export default function OmJTI() {
-  return <div></div>;
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>JTI overskrift</h1>
+      <div style={{ margin: 100 }}>
+        <div
+          style={{
+            padding: "200px 100px",
+            border: "1px solid grey",
+            boxShadow: "0 0 3px",
+          }}
+        >
+          <h2>
+            En video hvor du kort forklarer JTI her? Da kan vi vise frem
+            kvaliteten på kurs/ video og fiske inn kunder.
+          </h2>
+        </div>
+        <div>
+          <h2 style={{ textAlign: "center" }}>Mini overskrift?</h2>
+          <p>
+            Dine preferanser påvirker deg kanskje mer enn du er klar over! Har
+            du lyst til å forstå mer om hvordan dine preferanser påvirker deg,
+            dine handlinger og dine relasjoner? Da er Jung Type Index et godt
+            verktøy. JTI er ikke en personlighetstest, og den måler hverken
+            ferdigheter IQ eller EQ. JTI kartlegger og belyser den enkeltes-
+            eller teamets- preferanser og forskjeller på en analytisk måte.
+            Typeteorien i JTI kan få deg til å betrakte relasjoner, samhandling
+            og kommunikasjon på en ny og spennende måte. JTI som
+            relasjonsverktøy er nyttig for deg som enkeltperson, for ledere, for
+            grupper/ team og for hele organisasjoner. JTI er et anerkjent og
+            internasjonalt verktøy og er utviklet for individplan (JTI) og for
+            team/grupper (Teamkompasset). Begge verktøyene benyttes ofte i
+            forbindelse med organisasjonsutvikling og lederutvikling.
+          </p>
+        </div>
+        <div>
+          <p>
+            JTI måler menneskers preferanser langs fire ‘hovedlinjer’
+            (dimensjoner) i personligheten, med 8 preferanser fordelt på 4
+            dimensjoner
+          </p>
+          <h3 style={{ textAlign: "center", margin: 100 }}>
+            Bilde diagram av jti typer
+          </h3>
+          <p>
+            JTI måler preferanser og forskjeller. Ingen preferanser er bedre enn
+            andre, de er bare ulike. Det er denne ulikheten som gir dynamikk i
+            kommunikasjon og samhandling, og er grunnen til at relasjoner er
+            både fascinerende, krevende og givende.
+          </p>
+        </div>
+      </div>
+
+      <h1>
+        Korte litt ned på disse listene nedenfor? Det er litt mye tekst og det
+        kan være vanskelig å finne informasjon man er ute etter. Eventuelt
+        splitte denne opp i flere "komponenter/avsnitt" og ha egne overskrifter.
+      </h1>
+      <div className={styles.container}>
+        <ul className={styles.ul}>
+          <h3>Fakta om JTI</h3>
+          <h2>Ha et bilde her? et vektor icon for eksempel.</h2>
+          <li>
+            JTI bygger på Carl Jungs typeteori, og måler personlige preferanser
+            innen fire dimensjoner i personligheten (se under)
+          </li>
+          <li>JTI er et ikke evaluerende psykologisk verktøy</li>
+          <li>
+            Viser dine preferanser, hva du foretrekker når du står fritt til å
+            velge
+          </li>
+          <li>JTI belyser først og fremst styrker og forskjeller</li>
+          <li>Godt utprøvd, basert på grundig teori og grundig forskning</li>
+          <li>Laget for normale mennesker i normale livssituasjoner</li>
+          <li>Profesjonelt administrert og tolket</li>
+          <li>
+            Utviklet av Optimas org. Psykologer AS, fundamentert i Meyers Brigg
+          </li>
+          <li>
+            JTI finnes på følgende språk: Norsk, Svensk, Dansk, Engelsk,
+            Nederlandsk, Polsk, Tysk, Flamsk, Finsk, Estisk, Latvisk, Lithuansk,
+            Italiensk, Fransk, Spansk og Mandarin
+          </li>
+          <li>Kvalitetssikret av DNV-GL (Veritas)</li>
+        </ul>
+        <ul className={styles.ul}>
+          <h3> Hva kan JTI bidra med for enkeltpersoner</h3>
+          <h2>Ha et bilde her? et vektor icon for eksempel.</h2>
+          <li>
+            Sette pris på egne og andres spesielle evner og styrker. Lære
+            hvordan jeg skal utnytte mine og andres styrker
+          </li>
+          <li>
+            Å øke din lære om hvordan vi som mennesker oppfatter tilgjengelig
+            informasjon.
+          </li>
+          <li>Forstå hvordan du passer i din jobb</li>
+          <li>Yrkes og karrierevalg</li>
+          <li>
+            Diskutere dine reaksjoner og følelser ut fra et objektivt rammeverk
+          </li>
+          <li>Kommunisere mer effektivt med andre</li>
+          <li>Samarbeide bedre hjemme og på jobben</li>
+          <li>
+            Å forstå hvorfor noe faller lett for enkelte, og vanskelig for andre
+          </li>
+          <li>Forstå hvorfor man lettere finner tonen med enkelte</li>
+          <li>Å få en bedre motivasjon</li>
+          <li>
+            Bruke kunnskapen om personlighetstyper til å forstå deg selv og
+            dynamikken i relasjoner
+          </li>
+          <li>
+            Bli mer bevisst og kunne forbedre de områder jeg kanskje overser
+            eller som ikke faller så lett for meg
+          </li>
+        </ul>
+        <ul className={styles.ul}>
+          <h3> Hva kan JTI bidra med for organisasjonen</h3>
+          <h2>Ha et bilde her? et vektor icon for eksempel.</h2>
+          <li>
+            Få en logisk ordnet modell for menneskelig adferd, en teori som er
+            enkel å forstå
+          </li>
+          <li>
+            Få et logisk oversiktlig verktøy og en god tilnærming til
+            psykososiale temaer i bedriften
+          </li>
+          <li>Konflikthåndtering/ redusere uproduktive konflikter</li>
+          <li>Kompetanseutvikling / personlig utvikling</li>
+          <li>Relasjon og kommunikasjonsforståelse</li>
+          <li>Arbeidsmiljøutvikling</li>
+          <li>Læringsstil</li>
+          <li>Karriereutvikling og karriererådgivning</li>
+          <li>Teamutvikling</li>
+          <li>Lederutvikling</li>
+          <li>Ledelsesrådgivning</li>
+          <li>Identifisere styrker og muligheter i grupper</li>
+          <li>Bygge opp forståelse for normer og kultur</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
