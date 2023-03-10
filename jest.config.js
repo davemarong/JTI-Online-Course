cc7a4052339f1c63c0b68f1b8e2f3013d12cdd20
DaveTheBrave <kmarong@hotmail.com>
2023-03-10 20:29:17 +0100

    Rerfactor code

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
     "!**/node_modules/**",
   ],
   moduleNameMapper: {
-    /* Handle CSS imports (with CSS modules)
+    /* Handle CSS imports  (with CSS modules)
       https://jestjs.io/docs/webpack#mocking-css-modules */
     "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy",
 
